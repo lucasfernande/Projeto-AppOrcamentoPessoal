7282b4122400d3d087a714d30e3e5a68698c512a
lucasfernande <lucas.fernandes6192@outlook.com>
2020-10-19 15:07:05 -0300

    recuperando os dados dos formulários

---
+++ app.js
@@ -0,0 +1,8 @@
+function cadastrarDespesa() {
+	let ano = document.getElementById('ano')
+	let mes = document.getElementById('mes')
+	let dia = document.getElementById('dia')
+	let tipo = document.getElementById('tipo')
+	let descricao = document.getElementById('descricao')
+	let valor = document.getElementById('valor')
+}
