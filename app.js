e1390b1be9410055e218533ddbccfd0b5e223305
lucasfernande <lucas.fernandes6192@outlook.com>
2020-10-19 15:24:51 -0300

    criando a classe despesa

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
+class Despesa {
+	constructor(ano, mes, dia, tipo, valor, descricao) {
+		this.ano = ano
+		this.mes = mes
+		this.dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+	}
+}
+
 function cadastrarDespesa() {
+	// recuperando os dados do formulário de cadastro
 	let ano = document.getElementById('ano')
 	let mes = document.getElementById('mes')
 	let dia = document.getElementById('dia')
 	let tipo = document.getElementById('tipo')
 	let descricao = document.getElementById('descricao')
 	let valor = document.getElementById('valor')
+
+	// instanciando 
+	let despesa = new Despesa(
+		ano.value
+		mes.value
+		dia.value
+		tipo.value
+		descricao.value
+		valor.value
+	)
 }
